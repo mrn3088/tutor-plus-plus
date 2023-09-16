c6965bd4033736b5d494694611bcf3bfe02af55e
mrn3088 <r7ma3088@gmail.com>
2023-09-16 19:43:32 +0800

    feat: fixed book link

--- question_postprocessing.py
+++ question_postprocessing.py
@@ -9,7 +9,6 @@ def question_postprocessing(model_output, question, metadata_books, metadata_vid
         "\n\n### 模型回答: \n"
         "> {}\n\n"
         "#### 书籍索引:\n"
-        "- [查看书籍]({})\n"
         "- 索引文件名: {}\n"
         "- 索引页数: {}\n"
         "#### 视频索引:\n"
@@ -18,7 +17,6 @@ def question_postprocessing(model_output, question, metadata_books, metadata_vid
         "- 索引视频时间戳: {} min {} sec\n"
     ).format(
         model_output, 
-        'amazon.com',
         metadata_books['file_name'], 
         metadata_books['page_num'], 
         metadata_videos['video_id'], 
